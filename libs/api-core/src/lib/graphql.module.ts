fc87118942fee206696991b4746970fcc9f7d5f1
Fabeure <sazaouzi@gmail.com>
2025-05-15 18:17:11 +0100

    fix schema infinite rebuild

--- libs/api-core/src/lib/graphql.module.ts
+++ libs/api-core/src/lib/graphql.module.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
     imports: [
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
-            autoSchemaFile: join(process.cwd(), 'apps/TheySaid/src/schema.gql'),
+            autoSchemaFile: true, // join(process.cwd(), 'libs/api-core/src/schema.gql'), FIXME this causes infinite rebuilds when schema already exists :(
             sortSchema: true,
             graphiql: true,
         }),
