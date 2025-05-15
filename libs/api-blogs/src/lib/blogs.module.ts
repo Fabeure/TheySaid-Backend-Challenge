096781d0094d2b3be701903774899ad77915380f
Fabeure <sazaouzi@gmail.com>
2025-05-15 14:27:23 +0100

    configure graphql and typeorm

--- libs/api-blogs/src/lib/blogs.module.ts
+++ libs/api-blogs/src/lib/blogs.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { BlogsResolver } from './blogs.resolver';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [],
   providers: [BlogsResolver],
   exports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: true,
+      sortSchema: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ]
 })
 export class BlogsModule { }
