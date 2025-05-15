abbabc05652354ba65153990329c3496f66a50f8
Fabeure <sazaouzi@gmail.com>
2025-05-15 14:54:24 +0100

    update modules

--- apps/TheySaid/src/app/app.module.ts
+++ apps/TheySaid/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsModule } from '@myorg/api-blogs'
+import { ApiCoreModule } from '@myorg/api-core'
 
 @Module({
-  imports: [BlogsModule],
+  imports: [BlogsModule, ApiCoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/api-blogs/src/lib/blogs.module.ts
+++ libs/api-blogs/src/lib/blogs.module.ts
@@ -1,34 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BlogsResolver } from './blogs.resolver';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Blog } from '@myorg/api-models'
-import { join } from 'path';
 
 @Module({
   controllers: [],
   providers: [BlogsResolver],
   exports: [],
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      graphiql: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-    }),
-
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'test',
-      entities: [Blog],
-      synchronize: true,
-      autoLoadEntities: true,
-    }),
-  ]
 })
 export class BlogsModule { }
