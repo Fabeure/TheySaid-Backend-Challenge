ee410c8681769c1da9f64e7b5ec13d65e6205360
Fabeure <sazaouzi@gmail.com>
2025-05-15 14:26:36 +0100

    add typeorm and graphql packages

--- apps/TheySaid/src/app/app.module.ts
+++ apps/TheySaid/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogsModule } from '@myorg/api-blogs'
 
 @Module({
-  imports: [],
+  imports: [BlogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
